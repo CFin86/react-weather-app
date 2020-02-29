40699a1cad0661d15ade266f69baf63fdb87cf77
Chris Finney <chris.finney@pantheon.io>
2020-02-29 14:31:11 -0600

    push for lisa

--- src/index.js
+++ src/index.js
@@ -5,25 +5,28 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {lat: null, errorMessage: ''};
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-                console.log(position)
-            },
-            err => this.setState({ errorMessage: err.message })
-        )
+
     }
     
-    render() {
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position =>  this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
         if(this.state.errorMessage && !this.state.late){
             return (<div>Error: {this.state.lat}</div>
             )
         }
+    }
+
+    componentDidUpdate(){
         if(!this.state.errorMessage && this.state.lat){
             return (
             <div>Latitude: {this.state.lat} </div>
             );
         }
+    }
+    render() {
         return <div>Loading...</div>;
     }
 }
